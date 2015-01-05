7a8893206f9d760a1596aad8ddce63c278036b2a
Kofuku <iwona.gawenda@gmail.com>
2015-01-05 18:57:17 +0100

    GUI.py

---
+++ SaperPckg/GUI.py
@@ -0,0 +1,34 @@
+__author__ = 'igawenda'
+
+from tkinter import *
+from logic import Saper
+
+
+class Window():
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.parent.title("Saper")
+        Label(parent, text="New Game").grid(row=0)
+        frame = Frame(parent, background="black", height=200, width=300)
+        frame.grid(row=1)
+        self.center_window()
+
+    def center_window(self):
+        width = 300
+        height = 300
+        self_width = self.parent.winfo_screenwidth()
+        self_height = self.parent.winfo_screenheight()
+        x = (self_width - width)/2
+        y = (self_height - height)/2
+        self.parent.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+    def button_creator(self, col_count, row_count):
+        Saper.init_board()
+
+
+
+
+root = Tk()
+window = Window(root)
+root.mainloop()
---
+++ SaperPckg/logic.py
@@ -0,0 +1,85 @@
+__author__ = 'pkisielewicz'
+
+import random
+
+
+class Saper():
+    bombs = 10
+    board = {}
+    boardWidth = 8
+    boardHeight = 8
+
+    def init_board(self):
+        global board
+        for row in range(0, Saper.boardHeight):
+            for column in range(0, Saper.boardWidth):
+                board[row, column] = 0
+        print(board)
+
+    def generate_bombs(self):
+        global bombs
+        while bombs > 0:
+            bomb_col_place = random.randrange(0, Saper.boardWidth)
+            bomb_row_place = random.randrange(0, Saper.boardHeight)
+            if board[bomb_row_place, bomb_col_place] != 9:  # 9 means there is bomb on this place
+                board[bomb_row_place, bomb_col_place] = 9
+                Saper.count_bombs(self, bomb_row_place, bomb_col_place)
+                bombs -= 1
+
+    def count_bombs(self, bomb_row_place, bomb_col_place):
+        global board
+        if bomb_row_place > 0:
+            if bomb_col_place > 0:
+                board[bomb_row_place - 1, bomb_col_place - 1] += 1
+            board[bomb_row_place - 1, bomb_col_place] += 1
+            if bomb_col_place < (Saper.boardWidth - 1):
+                board[bomb_row_place - 1, bomb_col_place + 1] += 1
+        if bomb_row_place < (Saper.boardHeight - 1):
+            if bomb_col_place > 0:
+                board[bomb_row_place + 1, bomb_col_place - 1] += 1
+            board[bomb_row_place + 1, bomb_col_place] += 1
+            if bomb_col_place < (Saper.boardWidth - 1):
+                board[bomb_row_place + 1, bomb_col_place + 1] += 1
+        if bomb_col_place > 0:
+            board[bomb_row_place, bomb_col_place - 1] += 1
+        if bomb_col_place < (Saper.boardWidth - 1):
+            board[bomb_row_place, bomb_col_place + 1] += 1
+
+    def left_click(self, row, col):
+        global board
+        if board[row, col] >= 9:
+            Saper.show_all_bombs()
+        elif board[row, col] > 0:
+            Saper.left_click(row, col)# Placeholder
+        else:
+            # Pokazanie wszystkich wspólnych zer i otaczających numerów
+            if row > 0:
+                if col > 0:
+                    Saper.left_click(row-1, col - 1)
+                Saper.left_click(row-1, col)
+                if col < (Saper.boardWidth - 1):
+                    Saper.left_click(row-1, col + 1)
+            if row < (Saper.boardHeight - 1):
+                if col > 0:
+                    Saper.left_click(row+1, col - 1)
+                Saper.left_click(row+1, col)
+                if col < (Saper.boardWidth - 1):
+                    Saper.left_click(row+1, col + 1)
+            if col > 0:
+                Saper.left_click(row, col - 1)
+            if col < (Saper.boardWidth - 1):
+                Saper.left_click(row, col + 1)
+
+    def show_all_bombs(self):
+        for row in range(0, Saper.boardHeight):
+            for column in range(0, Saper.boardWidth):
+                if board[row, column] >= 9:
+                    Saper.left_click(row, column)# Placeholder
+
+    def right_click(row, col):
+        global board
+        # Ustawia flagę
+
+    init_board()
+    generate_bombs()
+    print(board)
