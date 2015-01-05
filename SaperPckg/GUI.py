6d711a02603f0f113bbfa3579a3b0323b49954bf
Kofuku <iwona.gawenda@gmail.com>
2015-01-06 00:32:01 +0100

    GUI.py modified - adjusting window size and grid.

--- SaperPckg/GUI.py
+++ SaperPckg/GUI.py
@@ -9,25 +9,15 @@ class Window():
     def __init__(self, parent):
         self.parent = parent
         self.parent.title("Saper")
-        Button(parent, text="New Game", relief=GROOVE).grid(row=0)
-        self.center_window()
+        Button(parent, text="New Game", relief=GROOVE).grid(row=0, columnspan=Saper.boardWidth, sticky=W)
         self.button_creator()
-
-    def center_window(self):
-        width = 275
-        height = 250
-        self_width = self.parent.winfo_screenwidth()
-        self_height = self.parent.winfo_screenheight()
-        x = (self_width - width)/2
-        y = (self_height - height)/2
-        self.parent.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
     def button_creator(self):
         col_count = Saper.boardWidth
         row_count = Saper.boardHeight
         for col in range(0, col_count):
             for row in range(0, row_count):
-                Button(self.parent, width=2, height=1, relief=GROOVE).grid(row=row+1, column=col+1)
+                Button(self.parent, width=2, height=1, relief=GROOVE).grid(row=row+1, column=col, padx=1, pady=1)
 
 root = Tk()
 root.resizable(0,0)
