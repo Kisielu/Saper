b6dfed889c17db7b31d2b1a9632d71ef90ad67ab
Kofuku <iwona.gawenda@gmail.com>
2015-01-06 21:16:45 +0100

    Repaired buttons.

--- SaperPckg/GUI.py
+++ SaperPckg/GUI.py
@@ -4,20 +4,27 @@ from tkinter import *
 from SaperPckg.logic import Saper
 
 
+saper = Saper()
+
+
 class Window():
 
     def __init__(self, parent):
         self.parent = parent
         self.parent.title("Saper")
-        Button(parent, text="New Game", relief=GROOVE).grid(row=0, columnspan=Saper.boardWidth, sticky=W)
         self.button_creator()
 
     def button_creator(self):
-        col_count = Saper.boardWidth
-        row_count = Saper.boardHeight
+        button = Button(self.parent, text="New Game", relief=GROOVE, command=saper.new_game)
+        button.grid(row=0, columnspan=saper.boardWidth, sticky=W, padx=2, pady=2)
+        col_count = saper.boardWidth
+        row_count = saper.boardHeight
         for col in range(0, col_count):
             for row in range(0, row_count):
-                Button(self.parent, width=2, height=1, relief=GROOVE).grid(row=row+1, column=col, padx=1, pady=1)
+                butt = Button(self.parent, width=2, height=1, relief=GROOVE, command=lambda x=row, y =col: saper.left_click(x, y))
+                butt.bind("<Button-3>", lambda x=row, y =col: saper.right_click(x, y))
+                butt.grid(row=row+1, column=col)
+
 
 root = Tk()
 root.resizable(0,0)
--- SaperPckg/logic.py
+++ SaperPckg/logic.py
@@ -4,83 +4,93 @@ import random
 
 
 class Saper():
-    bombs = 10
-    board = {}
-    boardWidth = 8
-    boardHeight = 8
+
+    #bombs = 10
+    #board = {}
+    #boardWidth = 8
+    #boardHeight = 8
+
+    def __init__(self):
+        self.bombs = 10
+        self.board = {}
+        self.boardWidth = 8
+        self.boardHeight = 8
 
     def init_board(self):
-        global board
-        for row in range(0, Saper.boardHeight):
-            for column in range(0, Saper.boardWidth):
-                board[row, column] = 0
-        print(board)
+        for row in range(0, self.boardHeight):
+            for column in range(0, self.boardWidth):
+                self.board[row, column] = 0
+        print(self.board)
 
     def generate_bombs(self):
-        global bombs
-        while bombs > 0:
-            bomb_col_place = random.randrange(0, Saper.boardWidth)
-            bomb_row_place = random.randrange(0, Saper.boardHeight)
-            if board[bomb_row_place, bomb_col_place] != 9:  # 9 means there is bomb on this place
-                board[bomb_row_place, bomb_col_place] = 9
-                Saper.count_bombs(self, bomb_row_place, bomb_col_place)
-                bombs -= 1
+        self.bombs = 10
+        while self.bombs > 0:
+            bomb_col_place = random.randrange(0, self.boardWidth)
+            bomb_row_place = random.randrange(0, self.boardHeight)
+            if self.board[bomb_row_place, bomb_col_place] != 9:  # 9 means there is bomb on this place
+                self.board[bomb_row_place, bomb_col_place] = 9
+                self.count_bombs(bomb_row_place, bomb_col_place)
+                self.bombs -= 1
+        print(self.board)
 
     def count_bombs(self, bomb_row_place, bomb_col_place):
-        global board
         if bomb_row_place > 0:
             if bomb_col_place > 0:
-                board[bomb_row_place - 1, bomb_col_place - 1] += 1
-            board[bomb_row_place - 1, bomb_col_place] += 1
-            if bomb_col_place < (Saper.boardWidth - 1):
-                board[bomb_row_place - 1, bomb_col_place + 1] += 1
-        if bomb_row_place < (Saper.boardHeight - 1):
+                self.board[bomb_row_place - 1, bomb_col_place - 1] += 1
+            self.board[bomb_row_place - 1, bomb_col_place] += 1
+            if bomb_col_place < (self.boardWidth - 1):
+                self.board[bomb_row_place - 1, bomb_col_place + 1] += 1
+        if bomb_row_place < (self.boardHeight - 1):
             if bomb_col_place > 0:
-                board[bomb_row_place + 1, bomb_col_place - 1] += 1
-            board[bomb_row_place + 1, bomb_col_place] += 1
-            if bomb_col_place < (Saper.boardWidth - 1):
-                board[bomb_row_place + 1, bomb_col_place + 1] += 1
+                self.board[bomb_row_place + 1, bomb_col_place - 1] += 1
+            self.board[bomb_row_place + 1, bomb_col_place] += 1
+            if bomb_col_place < (self.boardWidth - 1):
+                self.board[bomb_row_place + 1, bomb_col_place + 1] += 1
         if bomb_col_place > 0:
-            board[bomb_row_place, bomb_col_place - 1] += 1
-        if bomb_col_place < (Saper.boardWidth - 1):
-            board[bomb_row_place, bomb_col_place + 1] += 1
+            self.board[bomb_row_place, bomb_col_place - 1] += 1
+        if bomb_col_place < (self.boardWidth - 1):
+            self.board[bomb_row_place, bomb_col_place + 1] += 1
 
     def left_click(self, row, col):
-        global board
-        if board[row, col] >= 9:
-            Saper.show_all_bombs()
-        elif board[row, col] > 0:
-            Saper.left_click(row, col)# Placeholder
+        if self.board[row, col] >= 9:
+            self.show_all_bombs()
+        elif self.board[row, col] > 0:
+            pass# Placeholder
         else:
             # Pokazanie wszystkich wspólnych zer i otaczających numerów
-            if row > 0:
+            pass
+            """if row > 0:
                 if col > 0:
-                    Saper.left_click(self, row-1, col - 1)
-                Saper.left_click(self, row-1, col)
-                if col < (Saper.boardWidth - 1):
-                    Saper.left_click(self, row-1, col + 1)
-            if row < (Saper.boardHeight - 1):
+                    self.left_click(row-1, col - 1)
+                self.left_click(row-1, col)
+                if col < (self.boardWidth - 1):
+                    self.left_click(row-1, col + 1)
+            if row < (self.boardHeight - 1):
                 if col > 0:
-                    Saper.left_click(self, row+1, col - 1)
-                Saper.left_click(self, row+1, col)
-                if col < (Saper.boardWidth - 1):
-                    Saper.left_click(self, row+1, col + 1)
+                    self.left_click(row+1, col - 1)
+                self.left_click(row+1, col)
+                if col < (self.boardWidth - 1):
+                    self.left_click(row+1, col + 1)
             if col > 0:
-                Saper.left_click(self, row, col - 1)
-            if col < (Saper.boardWidth - 1):
-                Saper.left_click(self, row, col + 1)
+                self.left_click(row, col - 1)
+            if col < (self.boardWidth - 1):
+                self.left_click(row, col + 1)"""
 
     def show_all_bombs(self):
-        for row in range(0, Saper.boardHeight):
-            for column in range(0, Saper.boardWidth):
-                if board[row, column] >= 9:
-                    Saper.left_click(self, row, column)# Placeholder
+        for row in range(0, self.boardHeight):
+            for column in range(0, self.boardWidth):
+                if self.board[row, column] >= 9:
+                    pass# Placeholder
 
     def right_click(self, row, col):
-        global board
-        Saper.right_click(self, row, col)
+        print ("Right click")
         # Ustawia flagę
 
-    #Saper.init_board()
-    #Saper.generate_bombs()
-    #print(board)
+    def new_game(self):
+        self.init_board()
+        self.generate_bombs()
+
+#saper = Saper()
+
+#saper.new_game()
+
